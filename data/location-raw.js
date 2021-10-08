1774b2aeeac66b4485000d181d6bfefdf1169633
diyanamendoza <diyana.amp@pm.me>
2021-10-07 17:32:34 -0700

    Add lab 10 reqs

---
+++ data/location-raw.js
@@ -0,0 +1,193 @@
+const data = [
+  {
+    'place_id': '274163681',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '119557',
+    'boundingbox': [
+      '33.647808',
+      '33.886823',
+      '-84.551068',
+      '-84.28956'
+    ],
+    'lat': '33.7489924',
+    'lon': '-84.3902644',
+    'display_name': 'Atlanta, Fulton County, Georgia, USA',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.7908028207926616,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '275055851',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '3507044',
+    'boundingbox': [
+      '33.084104',
+      '33.145496',
+      '-94.229656',
+      '-94.123227'
+    ],
+    'lat': '33.1162131',
+    'lon': '-94.1663493',
+    'display_name': 'Atlanta, Cass County, Texas, 75551, USA',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.540714888027748,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '274302603',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '130165',
+    'boundingbox': [
+      '37.4308005',
+      '37.4404952',
+      '-96.7744392',
+      '-96.7596912'
+    ],
+    'lat': '37.4356784',
+    'lon': '-96.7691606',
+    'display_name': 'Atlanta, Cowley County, Kansas, 67008, USA',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.517511672002615,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '274272897',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '141187',
+    'boundingbox': [
+      '39.892874',
+      '39.904944',
+      '-92.48668',
+      '-92.473969'
+    ],
+    'lat': '39.898646',
+    'lon': '-92.4810208',
+    'display_name': 'Atlanta, Macon County, Missouri, 63530, USA',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.49269772601504613,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '257686693',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'way',
+    'osm_id': '825913152',
+    'boundingbox': [
+      '33.708663',
+      '33.7130349',
+      '-84.3715977',
+      '-84.3665827'
+    ],
+    'lat': '33.710511',
+    'lon': '-84.36899652238671',
+    'display_name': 'United States Penitentiary, Atlanta, 601, McDonough Boulevard Southeast, Thomasville, Atlanta, Fulton County, Georgia, 30315, USA',
+    'class': 'amenity',
+    'type': 'prison',
+    'importance': 0.4821522577273518,
+    'icon': 'https://locationiq.org/static/images/mapicons/amenity_prison.p.20.png'
+  },
+  {
+    'place_id': '108197716',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'way',
+    'osm_id': '42904130',
+    'boundingbox': [
+      '33.7592415',
+      '33.759635',
+      '-84.389074',
+      '-84.3876781'
+    ],
+    'lat': '33.75938645',
+    'lon': '-84.38836408372237',
+    'display_name': 'The Westin Peachtree Plaza, Atlanta, 210, Ted Turner Drive Northwest, Five Points, Atlanta, Fulton County, Georgia, 30303, USA',
+    'class': 'tourism',
+    'type': 'hotel',
+    'importance': 0.4624313575409157,
+    'icon': 'https://locationiq.org/static/images/mapicons/accommodation_hotel2.p.20.png'
+  },
+  {
+    'place_id': '197963',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'node',
+    'osm_id': '158411802',
+    'boundingbox': [
+      '38.688725',
+      '38.728725',
+      '-75.6979851',
+      '-75.6579851'
+    ],
+    'lat': '38.708725',
+    'lon': '-75.6779851',
+    'display_name': 'Atlanta, Sussex County, Delaware, 19933, USA',
+    'class': 'place',
+    'type': 'hamlet',
+    'importance': 0.4566603705359764,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_village.p.20.png'
+  },
+  {
+    'place_id': '274168583',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '135267',
+    'boundingbox': [
+      '44.992783',
+      '45.016966',
+      '-84.184041',
+      '-84.11884'
+    ],
+    'lat': '45.0047575',
+    'lon': '-84.15197019460344',
+    'display_name': 'Atlanta, Atlanta, Briley Township, Montmorency County, Michigan, 49709, USA',
+    'class': 'boundary',
+    'type': 'census',
+    'importance': 0.4549349340745128
+  },
+  {
+    'place_id': '275193849',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '9222557',
+    'boundingbox': [
+      '45.1437154',
+      '45.1747149',
+      '-64.5219571',
+      '-64.4817206'
+    ],
+    'lat': '45.1513902',
+    'lon': '-64.5040943',
+    'display_name': 'Atlanta, Municipality of the County of Kings, Kings County, Nova Scotia, Canada',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.44999999999999996,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  },
+  {
+    'place_id': '273971874',
+    'licence': 'https://locationiq.com/attribution',
+    'osm_type': 'relation',
+    'osm_id': '126199',
+    'boundingbox': [
+      '40.2520452',
+      '40.2742804',
+      '-89.2433876',
+      '-89.2170808'
+    ],
+    'lat': '40.2596504',
+    'lon': '-89.2332466',
+    'display_name': 'Atlanta, Logan County, Illinois, 61723, USA',
+    'class': 'boundary',
+    'type': 'administrative',
+    'importance': 0.4385505566535597,
+    'icon': 'https://locationiq.org/static/images/mapicons/poi_boundary_administrative.p.20.png'
+  }
+];
+
+module.exports = { data };
